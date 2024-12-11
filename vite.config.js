f4a26727b5f7f821a715083225b15097dffefb37
PebriadiX <pebriadi390@gmail.com>
2024-12-11 16:15:21 +0700

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,21 @@
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+import {globSync} from 'glob'
+const files = globSync([
+    './resources/js/pages/**/*.js',
+    './resources/js/vendor/**/*.js',
+]);
+
+export default defineConfig({
+    plugins: [
+        laravel({
+            input: [
+                ...files,
+                'resources/css/app.css',
+                'resources/css/login.css',
+                'resources/js/app.js',
+            ],
+            refresh: true,
+        }),
+    ],
+});
